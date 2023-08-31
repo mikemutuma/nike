c672c1b6bb61e447bd06f65d1f910bfdecb06d32
mikemutuma <mikemutuma664@gmail.com>
2023-08-31 10:58:43 +0300

    first commit

---
+++ src/sections/index.js
@@ -0,0 +1,19 @@
+import CustomerReview from './CustomerReview';
+import PopularProducts from './PopularProducts';
+import Footer from './Footer';
+import Services from './Services';
+import SpecialOffers from './SpecialOffers';
+import Subscribe from './Subscribe';
+import SuperQuality from './SuperQuality';
+import Hero from './Hero';
+
+export {
+    Hero,
+    CustomerReview,
+    Footer,
+    PopularProducts,
+    Services,
+    SpecialOffers,
+    Subscribe,
+    SuperQuality
+}
